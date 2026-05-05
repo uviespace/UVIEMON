6f0c13661e98f4bd3941ca8713c0d7c56483634c
Johnny Böhm <johnny.boehm@mailbox.org>
2026-05-05 14:44:39 +0200

    Changed standard to gnu17 to have strtok_r available

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -o uviemon *.c -L./lib/ftdi/build -lftd2xx -lreadline -lm -lbsd -Wall -std=c17
+gcc -o uviemon *.c -L./lib/ftdi/build -lftd2xx -lreadline -lm -lbsd -Wall -std=gnu17
